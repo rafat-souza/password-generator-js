9f19e3e28195329a0b3069d3fff3c6541d43d4aa
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-18 22:32:05 -0300

    random password generator configured

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Seleção de elementos
 
 const generatePasswordButton = document.querySelector("#generate-password");
-const generatedPasswordElement = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
 
 // Funções
 
@@ -22,8 +22,43 @@ const getSymbol = () => {
   return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
+const generatePassword = (
+  getLetterLowerCase,
+  getLetterUpperCase,
+  getNumber,
+  getSymbol,
+) => {
+  let password = "";
+
+  const passwordLength = 10;
+
+  const generators = [
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol,
+  ];
+
+  for (i = 0; i < passwordLength; i = i + 4) {
+    generators.forEach(() => {
+      const randomValue =
+        generators[Math.floor(Math.random() * generators.length)]();
+      password += randomValue;
+    });
+  }
+  password = password.slice(0, passwordLength);
+
+  generatedPasswordElement.style.display = "block";
+  generatedPasswordElement.querySelector("h4").innerText = password;
+};
+
 // Eventos
 
 generatePasswordButton.addEventListener("click", () => {
-  console.log("teste");
+  generatePassword(
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol,
+  );
 });
